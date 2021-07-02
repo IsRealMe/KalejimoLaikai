f9246be5ea97a2074cdccc7f7e71cd9d0278a6ef
IsRealMe <deividuuukas@gmail.com>
2021-07-02 11:11:58 +0200

    Math fix
    Sutvarkytas neteisingas bendros sumos apskaičiavimas.

--- script.js
+++ script.js
@@ -1,23 +1,15 @@
 let laikai;
 let bausmes = [];
 let bausmiuSkaicius = 0;
-let reikalingaData;
-let teistumuSkaicius = 0;
-let teistumaiMultiplier = 1;
-let lengvinanti = false;
 
 $().ready(function () {
-  $.getJSON("laikai.json", function (data) {
-    laikai = data;
-    for (let i = 0; i < data["kalejimoSarasas"].length; i++) {
+  $.getJSON("laikai.json", function (blaikas) {
+    laikai = blaikas;
+    for (let i = 0; i < blaikas["kalejimoSarasas"].length; i++) {
       $("#chooseContainer").append(
-        `<input type="checkbox" class="nusikaltimas" id=${i} value=${data[
-          "kalejimoSarasas"
-        ][i].value.toString()} />`,
-        `<label for=${i}>${data["kalejimoSarasas"][i].name}</label><br />`
+        `<input type="checkbox" class="nusikaltimas" id=${i} value=${blaikas["kalejimoSarasas"][i].value.toString()} />`,
+        `<label for=${i}>${blaikas["kalejimoSarasas"][i].name}</label><br />`
       );
-      reikalingaData = new Date(Date.now() - 1209600000);
-      $("#reikalingaData").html(reikalingaData.toLocaleDateString());
     }
     $(".nusikaltimas").change(function () {
       if (this.checked) {
@@ -30,18 +22,6 @@ $().ready(function () {
           }
         }
       }
-      recalculateBausme();
-    });
-    $("#lengvinantiButton").change(function () {
-      if (this.checked) {
-        lengvinanti = true;
-      } else {
-        lengvinanti = false;
-      }
-      recalculateBausme();
-    });
-    $(".teistumai").change(function () {
-      teistumaiMultiplier = laikai.teistumai[this.value - 1].value;
       recalculateBausme();
     });
   });
@@ -59,10 +39,7 @@ function recalculateBausme() {
       tempSuma += parseInt(bausme.value);
     }
 
-    suma =
-      suma * teistumaiMultiplier +
-      Math.ceil((tempSuma * teistumaiMultiplier) / 3);
-
+    suma = suma + tempSuma;
     $("#bausmeCount").html(suma);
   } else {
     $("#bausmeCount").html(0);
